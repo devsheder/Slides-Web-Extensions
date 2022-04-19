d4e0a2c9fdca41cc9ffc358ef1aa5b547c82121f
devsheder <lasselle.a@sfeir.com>
2022-04-19 20:06:45 +0200

    feat: manifest version execution tools in action

--- demo/js-apis/extension/background.js
+++ demo/js-apis/extension/background.js
@@ -10,19 +10,16 @@ chrome.runtime.onMessage.addListener(
         // receiving message from content_script
         console.log("background onMessage", request, sender, sendResponse);
         // sending message back to content_script
-        if (chrome.tabs.getAllInWindow) {
+        const manifestVersion = chrome.runtime.getManifest().manifest_version;
+        if (manifestVersion === 2) {
             // MV2
-            chrome.tabs.getAllInWindow(null, tabs => {
-                tabs.forEach(tab => {
-                    if (tab.url === "http://localhost:4444/index.html") {
-                        chrome.tabs.sendMessage(tab.id, "yo I'm background")
-                    }
-                });
+            chrome.tabs.query({url: "http://localhost:4444/index.html"}, result => {
+                chrome.tabs.sendMessage(result[0].id, "yo I'm the only background")
             });
         } else {
             //MV3
             const result = await chrome.tabs.query({url: "http://localhost:4444/index.html"})
-            chrome.tabs.sendMessage(result[0].id, "yo I'm background")
+            chrome.tabs.sendMessage(result[0].id, "yo I'm the only background")
         }
     }
 );
