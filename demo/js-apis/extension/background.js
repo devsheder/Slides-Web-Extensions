80e4289a6c625ce63633ed48e847b2528e0dbcd3
devsheder <lasselle.a@sfeir.com>
2022-04-22 11:02:02 +0200

    fix(js_apis): background send msg back to content_script caller

--- demo/js-apis/extension/background.js
+++ demo/js-apis/extension/background.js
@@ -6,20 +6,12 @@ chrome.runtime.onMessageExternal.addListener(
 );
 
 chrome.runtime.onMessage.addListener(
-    async (request, sender, sendResponse) => {
+    (request, sender, sendResponse) => {
         // receiving message from content_script
         console.log("background onMessage", request, sender, sendResponse);
-        // sending message back to content_script
-        const manifestVersion = chrome.runtime.getManifest().manifest_version;
-        if (manifestVersion === 2) {
-            // MV2
-            chrome.tabs.query({url: "http://localhost:4444/index.html"}, result => {
-                chrome.tabs.sendMessage(result[0].id, "yo I'm the only background")
-            });
-        } else {
-            //MV3
-            const result = await chrome.tabs.query({url: "http://localhost:4444/index.html"})
-            chrome.tabs.sendMessage(result[0].id, "yo I'm the only background")
-        }
+        // sending message back to content_script caller
+        chrome.tabs.sendMessage(sender.tab.id, "yo I'm the only background")
+
+        return true;
     }
 );
