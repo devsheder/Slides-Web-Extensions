506e4716361e83d51aa3c258755943c917b0608b
Anthony PENA <pena.a@sfeir.com>
2022-10-18 13:47:57 +0200

    feat(demo): add log on image click

--- demo/lego-website/src/App.tsx
+++ demo/lego-website/src/App.tsx
@@ -1,10 +1,21 @@
+import { MouseEvent, useCallback } from 'react';
 import './App.css'
+import { Logger } from './logger'
 
 function App() {
+  const onClick = useOnClick();
   return (
-    <div className="App">
+    <div className="App" onClick={onClick}>
     </div>
   )
+}
+
+function useOnClick() {
+  return useCallback((event: MouseEvent) => {
+    const x = event.clientX;
+    const y = event.clientY;
+    Logger.warn('click on image', { x, y })
+  }, []);
 }
 
 export default App
